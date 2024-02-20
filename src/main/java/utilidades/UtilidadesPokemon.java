c38ab92f8b5b7c01b022e48aa4b0cb5a529d427a
ljlopezSAFA <94695938+ljlopezSAFA@users.noreply.github.com>
2024-02-20 18:44:37 +0100

    Update UtilidadesPokemon.java
    Sin resolver

--- src/main/java/utilidades/UtilidadesPokemon.java
+++ src/main/java/utilidades/UtilidadesPokemon.java
@@ -16,60 +16,26 @@ public class UtilidadesPokemon {
 
     public static List<Pokemon> obtenerPokemonConTipos(List<Pokemon> pokemons, List<TipoPokemon> tipos){
 
-        List<Pokemon> coincedentesTipos = new ArrayList<>();
-
-        for(Pokemon pokemon : pokemons){
-            List<TipoPokemon> tipoPokemons = new ArrayList<>(pokemon.getTipos());
-            tipoPokemons.retainAll(tipos);
-
-            if(tipoPokemons.size() >0){
-                coincedentesTipos.add(pokemon);
-            }
-        }
-
-        return coincedentesTipos;
+       return new ArrayList<>();
 
     }
 
     public static List<Pokemon> obtenerPokemonConTipos1(List<Pokemon> pokemons, List<TipoPokemon> tipos){
 
-        return pokemons
-                .stream()
-                .filter(p-> p.getTipos().retainAll(tipos))
-                .collect(Collectors.toList());
+        return new ArrayList<>();
     }
 
 
 
     public static Map<TipoPokemon, List<Pokemon>> obtenerPokemonPurosPorTipo(List<Pokemon> pokemons){
 
-        Map<TipoPokemon, List<Pokemon>> mapa = new HashMap<>();
-
-        for(Pokemon pokemon : pokemons){
-
-            //Comprobar que el tipo es unico
-            if(pokemon.getTipos().size() == 1 ){
-                TipoPokemon tipo = pokemon.getTipos().get(0);
-
-                //Comprobar si el tipo está como clave en el mapa
-                if(mapa.containsKey(tipo)){
-                    mapa.get(tipo).add(pokemon);
-                }else{
-                    mapa.put(tipo, new ArrayList<>(List.of(pokemon)));
-                }
-            }
-        }
-
-        return mapa;
+        return new HashMap<>();
 
     }
 
     public static Map<TipoPokemon, List<Pokemon>> obtenerPokemonPurosPorTipo1(List<Pokemon> pokemons){
 
-        return pokemons
-                .stream()
-                .filter(p-> p.getTipos().size() ==1)
-                .collect(Collectors.groupingBy(p-> p.getTipos().get(0)));
+        return new HashMap<>();
 
     }
 
